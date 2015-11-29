68ea0b60c6b5982d3747d1d22025c9f8648076f0
tjavsi <soke_best@hotmail.com>
2015-11-29 23:27:19 +0100

    Manifest added some permissions

--- app/src/main/java/com/example/soke/openblamebox/Activities/LoginActivity.java
+++ app/src/main/java/com/example/soke/openblamebox/Activities/LoginActivity.java
@@ -1,7 +1,9 @@
-package com.example.soke.openblamebox;
+package com.example.soke.openblamebox.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.soke.openblamebox.R;
 
 public class LoginActivity extends AppCompatActivity {
 
