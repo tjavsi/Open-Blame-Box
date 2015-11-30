73a338cd392d6a03e148785eec05459cfd8fea04
tjavsi <soke_best@hotmail.com>
2015-11-30 01:20:34 +0100

    Added at bit to the login screen

--- app/src/main/java/com/example/soke/openblamebox/Activities/LoginActivity.java
+++ app/src/main/java/com/example/soke/openblamebox/Activities/LoginActivity.java
@@ -4,12 +4,57 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.soke.openblamebox.R;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+
+        faceBookLoginActions();
+
         setContentView(R.layout.activity_login);
+    }
+    public void faceBookLoginActions() {
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        AccessTokenTracker mFacebookAccessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                LoginActivity.this.onFacebookAccessTokenChange(currentAccessToken);
+            }
+        };
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+            }
+        });
+    }
+
+    private void onFacebookAccessTokenChange(AccessToken currentAccessToken) {
+
     }
 }
